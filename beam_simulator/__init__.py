ed08e4d1b072d05bc17b3ab9d81e7e03db6851f3
cdilullo <cdilullo@unm.edu>
2021-02-22 22:23:25 -0700

    Restructured things for importability. Added __init__.py and setup.py

---
+++ beam_simulator/__init__.py
@@ -0,0 +1,10 @@
+"""
+Beam Simulator
+
+Provided modules:
+ * beam_simulator.nec
+ * beam_simulator.station
+ * beam_simulator.beamformer
+"""
+
+__version__ = '1.0'
---
+++ setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name = "beam_simulator",
+    version = "1.0",
+    description = "Beam Simulator",
+    long_description=long_description,
+    author = "Christopher DiLullo",
+    author_email = "cdilullo@unm.edu",
+    url = "https://github.com/cdilullo/beam_simulator",
+    license="GPL",
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                   'Topic :: Scientific/Engineering:: Astronomy',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.6',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Operating System :: POSIX :: Linux'],
+    packages = find_packages(),
+    python_requires = '>=3.6',
+    install_requires = ['aipy>=3.0.1', 'numpy>=1.19', 'scipy>=1.6', 'astropy>=4.2',
+                        'matplotlib>=3.3.2','numba>=0.51.2']
+    )
